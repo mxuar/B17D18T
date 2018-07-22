dbf3d04a2fe3dc74731b09059a943e63bf84810f
mxuar0562 <36626542+mxuar@users.noreply.github.com>
2018-07-22 11:45:35 +0800

    Update preprocess.py

--- Future Strategy 5011/preprocess.py
+++ Future Strategy 5011/preprocess.py
@@ -8,7 +8,7 @@ Created on Wed Apr 25 21:18:55 2018
 
 # directory below on your own computer accordingly
 import os
-working_directory = '*/SVM'
+working_directory = './Future Strategy 5011'
 os.chdir(working_directory)
 from svmtest import handle_bar
 
@@ -20,13 +20,13 @@ import empyrical.utils
 import pandas as pd
 import numpy as np
 from copy import deepcopy
-format2_dir = 'Data/data_format2_20170717_20170915.h5'
-f3='Data/1.h5'
+format2_dir = './Future Strategy 5011/Data/data_format2_20170717_20170915.h5'
+f3='./Data/data_format2_20170918_20171211.h5'
 
 
 btData = h5py.File(f3, mode='r')
 
 import glob
 
-f=h5py.File('Data/data_format2_20170918_20171211.h5', mode= "r")
+f=h5py.File('./Future Strategy 5011/Data/data_format2_20170918_20171211.h5', mode= "r")
     
